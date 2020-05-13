56f0cc0002e4de58d178d5aa433225729ca03878
Troy Domio <troyakins99@yahoo.com>
2020-05-13 22:53:59 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,11 +1,26 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => 
+      {},
+      :matriarch => 
+      {},
+      :hero => 
+      {},
+      :hero_friends =>
+      []
       }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+      :capulet => {
+        :partiarch =>
+        {},
+        :matriarch => 
+        {},
+        :heroine => 
+        {},
+        :heroine_friends => 
+        []
+      }
+  }
 
   
 end
